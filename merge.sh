930f88a83ce7354b7aaf1ab09c0a527395c5f8c4
poiigzhao <poiigzhao@rektec.com.cn>
2025-01-14 23:03:59 +0800

    增加merge脚本，支持将其他源地址的IP添加上地区标签，支持LInux与Windows脚本

---
+++ merge.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 下载文件并添加标签
+curl -s https://raw.githubusercontent.com/5yfx/CF_HK_US_EU_IP/refs/heads/main/%E7%BE%8E%E5%9B%BD.txt | sed 's/\(.*\)/\1#US-\1/' >us.txt
+curl -s https://raw.githubusercontent.com/5yfx/CF_HK_US_EU_IP/refs/heads/main/%E8%8B%B1%E5%9B%BD.txt | sed 's/\(.*\)/\1#英国-\1/' >uk.txt
+curl -s https://raw.githubusercontent.com/5yfx/CF_HK_US_EU_IP/refs/heads/main/%E9%A6%99%E6%B8%AF.txt | sed 's/\(.*\)/\1#HK-\1/' >hk.txt
+
+# 合并文件
+cat us.txt uk.txt hk.txt >result.txt
+
+# 清理临时文件
+rm us.txt uk.txt hk.txt
+
+# 提交到git
+git add result.txt
+git commit -m "Update IP list $(date +'%Y-%m-%d %H:%M:%S')"
+git push
